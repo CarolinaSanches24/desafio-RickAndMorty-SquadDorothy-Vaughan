923d5eb95b38a1b58ab1d381d3c3bbd2ef4fad2e
ThaisAp10 <thaisapare.10@gmail.com>
2024-02-19 13:53:14 -0300

    Inserindo o início da criação da rota do perfil do episódio

--- app.py
+++ app.py
@@ -5,30 +5,30 @@ app = Flask (__name__)
 
 @app.route("/") # rota de  URL raiz
 def get_list_characters_page():
-    url = "https://rickandmortyapi.com/api/character";
+    url = "https://rickandmortyapi.com/api/character"
     response = urllib.request.urlopen(url) # envia a req e recebe a res
-    data = response.read(); # leitura dos dados vindos da api
-    dict = json.loads(data); # transforma esses dados em json p/ python
+    data = response.read() # leitura dos dados vindos da api
+    dict = json.loads(data) # transforma esses dados em json p/ python
     
     return render_template("characters.html", characters = dict["results"])
 
 @app.route("/profile/<id>") # obter um personagem
 
 def get_profile(id):
-    url = "https://rickandmortyapi.com/api/character/"+id;
+    url = "https://rickandmortyapi.com/api/character/"+id
     response = urllib.request.urlopen(url) 
-    data = response.read(); 
-    dict = json.loads(data);
+    data = response.read() 
+    dict = json.loads(data)
     
     return render_template("profile.html", profile = dict)
 
 @app.route("/lista")
 
 def get_list_characters():
-    url = "https://rickandmortyapi.com/api/character";
+    url = "https://rickandmortyapi.com/api/character"
     response = urllib.request.urlopen(url)
-    characters = response.read();
-    dict = json.loads(characters);
+    characters = response.read()
+    dict = json.loads(characters)
     
     characters = []
     
@@ -38,6 +38,17 @@ def get_list_characters():
             "status":character["status"]
         }
         
-        characters.append(character);
+        characters.append(character)
     return {"characters":characters}
+
+# Rota carregar o perfil do episódio
+@app.route("/episode/<id>")
+def get_profile_episodes(id):
+    url = "https://rickandmortyapi.com/api/episode/"+id
+    response = urllib.request.urlopen(url) 
+    data = response.read() 
+    dict = json.loads(data)
+
+
+    return render_template("episode.html", episode = dict)
 
